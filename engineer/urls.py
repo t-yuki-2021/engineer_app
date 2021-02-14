23a11fe1e9ee73c11a196671c6d60cc00f042705
t-yuki-2021 <yuki.tanio0628@gmail.com>
2021-02-14 19:51:00 +0900

    教材と勉強メモの登録実装

--- engineer/urls.py
+++ engineer/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('users', views.users, name='users'),
     path('set_profile', views.set_profile, name='set_profile'),
     path('edit_profile/<int:num>', views.edit_profile, name='edit_profile'),
+    path('register_book', views.register_book, name='register_book'),
+    path('register_study/<int:num>/<int:is_public>', views.register_study, name='register_study'),
 ]
