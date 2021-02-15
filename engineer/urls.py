d29bd0474bb7d89c2fb7c9bb8d189a82ee674ce4
t-yuki-2021 <yuki.tanio0628@gmail.com>
2021-02-15 18:03:10 +0900

    マイページ作成

--- engineer/urls.py
+++ engineer/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('edit_profile/<int:num>', views.edit_profile, name='edit_profile'),
     path('register_book', views.register_book, name='register_book'),
     path('register_study/<int:num>/<int:is_public>', views.register_study, name='register_study'),
+    path('my_page', views.my_page, name='my_page'),
 ]
